1a06e85f75f2d730623545d1c58553455d111739
snooprat <starpan@gmail.com>
2018-05-25 12:22:56 +0800

    fix(scurses): panel hide and show make top_panel properly.

--- spck/panel.py
+++ spck/panel.py
@@ -1,53 +1,64 @@
 # scurses panel module.
 
 import spck.unicurses as uc
-from spck.scurses import WinObj
-
-
-global last_panel
-last_panel = None
 
 
 # Panel functions
 update_panels = uc.update_panels
 
 
+global last_panel
+last_panel = None
+
+
 def new_panel(win):
     global last_panel
-    last_panel = uc.new_panel(win.scr)
+    np = uc.new_panel(win.scr)
+    last_panel = PanObj(np, win)
+    uc.set_panel_userptr(np, last_panel)
     return last_panel
 
 
 def top_panel():
     global last_panel
-    if last_panel:
-        while last_panel.above():
-            last_panel = last_panel.above()
-    return last_panel
+    tp = last_panel
+    while tp and tp.above():
+        tp = tp.above()
+    return tp
 
 
 def bottom_panel():
     global last_panel
-    if last_panel:
-        while last_panel.below():
-            last_panel = last_panel.below()
-    return last_panel
+    bp = last_panel
+    while bp and bp.below():
+        bp = bp.below()
+    return bp
 
 
 # Functions based on Panel Object
 class PanObj(object):
     """Panel object from curses"""
-    def __init__(self, panel):
+    def __init__(self, panel, win):
         self.panel = panel
+        self._win = win
+        self._userptr = None
 
     def __del__(self):
         return uc.del_panel(self.panel)
 
     def above(self, *args, **kwds):
-        return uc.panel_above(self.panel, *args, **kwds)
+        pa = uc.panel_above(self.panel, *args, **kwds)
+        if pa is None:
+            return None
+        else:
+            return uc.panel_userptr(pa)
 
     def below(self, *args, **kwds):
-        return uc.panel_below(self.panel, *args, **kwds)
+        pb = uc.panel_below(self.panel, *args, **kwds)
+        if pb is None:
+            return None
+        else:
+            return uc.panel_userptr(pb)
 
     def bottom(self, *args, **kwds):
         return uc.bottom_panel(self.panel, *args, **kwds)
@@ -56,6 +67,13 @@ class PanObj(object):
         return uc.panel_hidden(self.panel, *args, **kwds)
 
     def hide(self, *args, **kwds):
+        global last_panel
+        if self.above():
+            last_panel = self.above()
+        elif self.below():
+            last_panel = self.below()
+        else:
+            last_panel = None
         return uc.hide_panel(self.panel, *args, **kwds)
 
     def move(self, *args, **kwds):
@@ -64,17 +82,19 @@ class PanObj(object):
     def replace(self, win):
         return uc.replace_panel(self.panel, win.scr)
 
-    def set_userptr(self, *args, **kwds):
-        return uc.set_panel_userptr(self.panel, *args, **kwds)
+    def set_userptr(self, obj):
+        self._userptr = obj
 
     def show(self, *args, **kwds):
+        global last_panel
+        last_panel = self
         return uc.show_panel(self.panel, *args, **kwds)
 
     def top(self, *args, **kwds):
         return uc.top_panel(self.panel, *args, **kwds)
 
-    def userptr(self, *args, **kwds):
-        return uc.panel_userptr(self.panel, *args, **kwds)
+    def userptr(self):
+        return self._userptr
 
-    def window(self, *args, **kwds):
-        return WinObj(uc.panel_window(self.panel, *args, **kwds))
+    def window(self):
+        return self._win
